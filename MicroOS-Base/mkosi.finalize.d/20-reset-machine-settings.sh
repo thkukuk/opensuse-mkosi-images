71b7f25160022e3cdae1ed2ea28bade8e7fd0cc8
Thorsten Kukuk <kukuk@suse.com>
2026-01-20 16:21:42 +0100

    Move MicroOS images to own git repo

--- MicroOS-Base/mkosi.finalize.d/20-reset-machine-settings.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/bash
-
-echo "*** reset machine settings and cache ***"
-#rm -fv /buildroot/etc/machine-id
-rm -fv /buildroot/var/lib/zypp/AnonymousUniqueId \
-       /buildroot/var/lib/systemd/random-seed
-rm -fv /buildroot/init
-rm -rfv /buildroot/var/cache/{gio-2.0,man,zypp}
