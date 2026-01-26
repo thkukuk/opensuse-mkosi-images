848b3047df76f1f4b77e3dde7ba83f8b530654f9
Thorsten Kukuk <kukuk@suse.com>
2026-01-26 09:44:45 +0100

    Merge Tumbleweed and MicroOS into one git repo

---
+++ MicroOS/mkosi.microos/mkosi.postinst.d/10-config.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/bash
+
+#
+# Setup configs and services
+#
+echo FONT="eurlatgr.psfu" >> /buildroot/etc/vconsole.conf
+
+echo "/etc /etc none bind,x-initrd.mount 0 0" >> /buildroot/etc/fstab
+
+# Adjust relabel file from SELinux
+test -f /buildroot/.autorelabel && mv -v /buildroot/.autorelabel /buildroot/etc/selinux/
+
+# Adjust zypp conf XXX
+#sed -i 's/^multiversion =.*/multiversion =/g' /buildroot/etc/zypp/zypp.conf
+#sed -i 's/.*solver.onlyRequires.*/solver.onlyRequires = true/g' /buildroot/etc/zypp/zypp.conf
+#sed -i 's/.*rpm.install.excludedocs.*/rpm.install.excludedocs = yes/g' /buildroot/etc/zypp/zypp.conf
